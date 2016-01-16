91b989fa26168008b6f92b9c5ea8bfc309edb5f7
agwan786 <agwan786@yahoo.com>
2016-01-16 09:47:03 +0530

    Make config route for every modules

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'pages';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$modules_path = APPPATH.'modules/';     
+$modules = scandir($modules_path);
+
+foreach($modules as $module)
+{
+    if($module === '.' || $module === '..') continue;
+    if(is_dir($modules_path) . '/' . $module)
+    {
+        $routes_path = $modules_path . $module . '/config/routes.php';
+        if(file_exists($routes_path))
+        {
+            require($routes_path);
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
